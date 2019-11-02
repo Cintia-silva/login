a37585b8fa79e292dc841565c12c4b8a7c9f4855
cintia <cintia_suelem-silva@estudante.sc.senai.br>
2019-11-02 00:31:00 -0200

    carrosel
    carrosel

--- perfil.php
+++ perfil.php
@@ -1,5 +1,6 @@
 <?php
 require_once "session.php";
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -29,6 +30,10 @@ require_once "session.php";
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Destaques</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="fotos.php">Fotos</a>
                 </li>
 
                 <li class="nav-item dropdown">
@@ -68,16 +73,16 @@ require_once "session.php";
                     <h2 class="text-center">
                         Registrado em: <?= $dataCriado ?>
                     </h2>
-                    </div>
                 </div>
             </div>
-            <!-- fim do jumbotron -->
+        </div>
+        <!-- fim do jumbotron -->
 
-            <!-- JavaScript (Opcional) -->
-            <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <!-- JavaScript (Opcional) -->
+        <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 </body>
 
 </html>
